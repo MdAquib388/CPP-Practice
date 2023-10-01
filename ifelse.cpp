cc84c6f864d950231a69e5b05a19f3ca71d20b73
Md Aquib <mdaquib.official@gmail.com>
2023-10-02 02:08:19 +0530

    ifElse

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int b=30;
+
+    if(a>b){
+        cout<<a<<" is greater than "<< b << endl;
+    }
+    else{
+        cout<<b<<" is greater than "<< a << endl;
+
+    }
+    return 0;
+}
