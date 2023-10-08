7073b078b084743ec4153ea5524559e9c6550908
Md Aquib <mdaquib.official@gmail.com>
2023-10-08 23:59:55 +0530

    Pattern programs

---
+++ pattern14.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout<<"Enter any no"<<endl;
+    cin>>n;
+     char start = 'A';
+    int row =1;
+    while (row <= n)
+    {
+        int col =1;
+        while (col <= row)
+        {
+            cout<<start<<" ";
+            start =start +1;
+            col =col+1;
+        }
+        cout<<endl;
+        row = row +1;
+    }
+    return 0;
+    
+}
